93e94c541ab8abc57e207f4cc2337263deee22dc
Prinata Rakha Santoso <prinatarakha@gmail.com>
2023-05-22 14:42:17 +0700

    Implementing user's repository

--- repositories/user_repository/user_pg/pg.go
+++ repositories/user_repository/user_pg/pg.go
@@ -4,6 +4,8 @@ import (
 	"final-project-3/models"
 	"final-project-3/pkg/errs"
 	"final-project-3/repositories/user_repository"
+	"fmt"
+	"log"
 
 	"gorm.io/gorm"
 )
@@ -17,17 +19,47 @@ func NewUserPG(db *gorm.DB) user_repository.UserRepository {
 }
 
 func (u *userPG) RegisterUser(newUser *models.User) (*models.User, errs.MessageErr) {
+	if err := u.db.Create(newUser).Error; err != nil {
+		log.Println(err.Error())
+		message := fmt.Sprintf("Failed to register a new user with Email %s", newUser.Email)
+		error := errs.NewInternalServerError(message)
+		return nil, error
+	}
 
+	return newUser, nil
 }
 
 func (u *userPG) GetUserByEmail(user *models.User) errs.MessageErr {
+	err := u.db.Where("email = ?", user.Email).Take(&user).Error
+	// Karna di Take, objek user akan terupdate, termasuk passwordnya.
+	// Makannya kita simpen dulu password dari request nya di service level.
 
+	if err != nil {
+		err2 := errs.NewBadRequest("Wrong email/password")
+		return err2
+	}
+
+	return nil
 }
 
 func (u *userPG) UpdateUser(user *models.User) (*models.User, errs.MessageErr) {
+	err := u.db.Model(user).Updates(user).Error
 
+	if err != nil {
+		err2 := errs.NewBadRequest(err.Error())
+		return nil, err2
+	}
+
+	return user, nil
 }
 
 func (u *userPG) DeleteUser(id uint) errs.MessageErr {
+	err := u.db.Delete(&models.User{}, id).Error
 
+	if err != nil {
+		err3 := errs.NewInternalServerError(err.Error())
+		return err3
+	}
+
+	return nil
 }
